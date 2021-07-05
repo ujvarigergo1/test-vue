12a4cc3b1f0c66065a7f4650f5faa88cca5068c3
Ujvári Gergő <ujvarigergo1@gmail.com>
2021-07-05 15:00:38 +0200

    Táblázat hozzáadva

--- firstvuecucc/src/App.vue
+++ firstvuecucc/src/App.vue
@@ -5,6 +5,32 @@
       <button @click="test()" :disabled="nemLezarhato">Add</button>
     </div>
     <label v-if="szovegLatszik">Az ertek jelenleg: {{ertek}}</label>
+
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th>id</th>
+          <th>oldVal</th>
+          <th>newVal</th>
+          <th>diff</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(adat, index) in this.tabla" :key="index" :row="adat">
+          <th scope="row">{{ adat.id }}</th>  
+          <td>{{ adat.oldVal }}</td> 
+          <td>{{ adat.newVal }}</td>
+          <td>{{ adat.diff }}</td> 
+          <td>
+            <input type="button" value="Delete" @click="deleteRow(index)"/>
+          </td>
+        </tr>
+      </tbody>
+      <tfoot>
+        Összesen: {{this.tabla.length}}
+      </tfoot>
+   </table>
   </div>
 </template>
 
@@ -17,18 +43,29 @@ export default {
       return {
         ertek: 0,
         inp: 0,
-        szovegLatszik: false
+        szovegLatszik: false,
+        tabla: []
       }
   },
   methods: {
     test() {
+      const date = new Date;
+      this.tabla.push({
+        oldVal: this.ertek,
+        newVal: this.ertek + this.inp,
+        diff: this.inp,
+        id: `${date.getDay()}-${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`})
+
       this.ertek += this.inp;
       this.szovegLatszik = true;
+    },
+    deleteRow(i) {
+      this.tabla.splice(i,1)
     }
   },
   computed: {
     nemLezarhato() {
-      return this.inp == 0 || this.imp == ''
+      return this.inp == 0 || this.inp == ''
     }
   }
 }

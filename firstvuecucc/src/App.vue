541a2e24f0af86f46428dce7f5e5029e233c52b2
Ujvári Gergő <ujvarigergo1@gmail.com>
2021-07-05 12:05:48 +0200

    first commit

---
+++ firstvuecucc/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div orientation='vertical'>
+    <input v-model.number="inp" type="number">
+    <button @click="test()">Teszt</button>
+    <label v-if="ertek != 0">Az ertek megvaltozott a kovetkezore: {{ertek}}</label>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+      return {
+        ertek: 0,
+        inp: 0
+      }
+    },
+     methods: {
+    test() {
+      this.ertek += this.inp
+      
+    }
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
